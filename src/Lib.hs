e2661e8fbbb722bd02a2913178000eb646a10e58
pe200012 <1326263755@qq.com>
2021-03-08 12:52:40 +0800

    BASIC: Arithmetic expr typecheck

--- src/Lib.hs
+++ src/Lib.hs
@@ -47,3 +47,47 @@ data Type = TINTEGER | TSTRING deriving (Show, Eq)
 data Value = I Int | S Text deriving (Show, Eq)
 
 makeBaseFunctor ''Expr
+
+{-
+>>> typecheck (VAR "A")
+Left "Variable not in scope: A"
+
+>>> typecheck (LET "A" (STR "bbb") (VAR "A"))
+Right TSTRING
+
+>>> typecheck (ADD (INT 0) (INT 2))
+Right TINTEGER
+
+>>> typecheck (ADD (INT 0) (STR "a"))
+Left "ADD expected NUMBER type but got TSTRING"
+
+>>> typecheck (ADD (STR "b") (STR "a"))
+Left "ADD expected NUMBER type but got TSTRING"
+
+-}
+
+typecheck :: Expr -> Either Text Type
+typecheck = runExcept . flip evalStateT empty . cata go
+  where
+    checkNumeric a b op = do
+        a' <- a
+        b' <- b
+        case (a', b') of
+            (TINTEGER, TINTEGER) -> return TINTEGER
+            (TINTEGER, _       ) -> throwError (format "{} expected NUMBER type but got {}" [op, show @Text b'])
+            _                    -> throwError (format "{} expected NUMBER type but got {}" [op, show @Text a'])
+    go :: (MonadState (HashMap Text Type) m, MonadError Text m) => ExprF (m Type) -> m Type
+    go (INTF _          ) = return TINTEGER
+    go (STRF _          ) = return TSTRING
+    go (VARF name       ) = maybe (throwError (format "Variable not in scope: {}" [name])) return . lookup name =<< get
+    go (LETF name t body) = t >>= modify . insert name >> body
+    go (NEGF t          ) = do
+        t' <- t
+        case t' of
+            TINTEGER -> return TINTEGER
+            _        -> throwError (format "Cannot match NUMBER type with {}" [show @Text t'])
+    go (ADDF a b) = checkNumeric a b "ADD"
+    go (SUBF a b) = checkNumeric a b "SUB"
+    go (MULF a b) = checkNumeric a b "MUL"
+    go (DIVF a b) = checkNumeric a b "DIV"
+    go (POWF a b) = checkNumeric a b "POW"
